ea32adfaa5379e76304f9dbfbb61f1a736fc25c5
kusi <lawcubegsm@gmail.com>
2022-11-03 16:51:25 +0000

    Fix linter error

--- morse_code.rb
+++ morse_code.rb
@@ -1,69 +1,68 @@
 # rubocop:disable Metrics/MethodLength
-# get each character
-$code_dict = {
-  'a' => '.-',
-  'b' => '-...',
-  'c' => '-.-.',
-  'd' => '-..',
-  'e' => '.',
-  'f' => '..-.',
-  'g' => '--.',
-  'h' => '....',
-  'i' => '..',
-  'j' => '.---',
-  'k' => '-.-',
-  'l' => '.-..',
-  'm' => '--',
-  'n' => '-.',
-  'o' => '---',
-  'p' => '.--.',
-  'q' => '--.-',
-  'r' => '.-.',
-  's' => '...',
-  't' => '-',
-  'u' => '..-',
-  'v' => '...-',
-  'w' => '.--',
-  'x' => '-..-',
-  'y' => '-.--',
-  'z' => '--..',
-  ' ' => ' ',
-  '1' => '.----',
-  '2' => '..---',
-  '3' => '...--',
-  '4' => '....-',
-  '5' => '.....',
-  '6' => '-....',
-  '7' => '--...',
-  '8' => '---..',
-  '9' => '----.',
-  '0' => '-----'
-}
 
-#get each character
 def decode_char(mychar)
- return  $code_dict.key(mychar).upcase
+  code_dict = {
+    'a' => '.-',
+    'b' => '-...',
+    'c' => '-.-.',
+    'd' => '-..',
+    'e' => '.',
+    'f' => '..-.',
+    'g' => '--.',
+    'h' => '....',
+    'i' => '..',
+    'j' => '.---',
+    'k' => '-.-',
+    'l' => '.-..',
+    'm' => '--',
+    'n' => '-.',
+    'o' => '---',
+    'p' => '.--.',
+    'q' => '--.-',
+    'r' => '.-.',
+    's' => '...',
+    't' => '-',
+    'u' => '..-',
+    'v' => '...-',
+    'w' => '.--',
+    'x' => '-..-',
+    'y' => '-.--',
+    'z' => '--..',
+    ' ' => ' ',
+    '1' => '.----',
+    '2' => '..---',
+    '3' => '...--',
+    '4' => '....-',
+    '5' => '.....',
+    '6' => '-....',
+    '7' => '--...',
+    '8' => '---..',
+    '9' => '----.',
+    '0' => '-----'
+  }
+
+  code_dict.key(mychar).upcase
 end
+# rubocop:enable Metrics/MethodLength
 
-
-#get each word
+# get each word
 def decode_morse_words(codes)
- codelist = codes.split(' ')
- word = codelist.map { |code|
-  decode_char(code)
- }
+  codelist = codes.split
+
+  word = codelist.map { |code|
+    decode_char(code)
+  }
   word.join
 end
 
-#combine words in message
+# combine words in message
 def morse_message(code)
- codelist = code.split('  ')
- message = codelist.map { |code|
-  decode_morse_words(code)
- }
- message.join(' ')
+  codelist = code.split('  ')
+  message = codelist.map { |code|
+    decode_morse_words(code)
+  }
+  message.join(' ')
 end
 
 # Returns A BOX FULL OF RUBIES
-puts morse_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
+puts morse_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
