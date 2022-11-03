87369a43608997c15ab3e4b3b6aeb8072b880bfa
kusi <lawcubegsm@gmail.com>
2022-11-03 16:17:17 +0000

    Update morse_code file
    - Add word decode method
    - Add message decode method

--- morse_code.rb
+++ morse_code.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/MethodLength
 # get each character
 $code_dict = {
   'a' => '.-',
@@ -39,8 +40,30 @@ $code_dict = {
   '0' => '-----'
 }
 
+#get each character
 def decode_char(mychar)
-  $code_dict.key(mychar).upcase
+ return  $code_dict.key(mychar).upcase
 end
 
-puts decode_char('.-')
+
+#get each word
+def decode_morse_words(codes)
+ codelist = codes.split(' ')
+ word = codelist.map { |code|
+  decode_char(code)
+ }
+  word.join
+end
+
+#combine words in message
+def morse_message(code)
+ codelist = code.split('  ')
+ message = codelist.map { |code|
+  decode_morse_words(code)
+ }
+ message.join(' ')
+end
+
+# Returns A BOX FULL OF RUBIES
+puts morse_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+
